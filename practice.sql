27a49c8e561394557ebe80a3e966585075f70804
galactickem <a00827581@tec.mx>
2023-05-04 17:03:01 -0600

    queries pt.1

---
+++ practice.sql
@@ -0,0 +1,18 @@
+-- DVD Rental practice
+SELECT * FROM payment;
+
+SELECT customer_id, SUM(amount) FROM payment
+GROUP BY customer_id
+ORDER BY SUM(amount) DESC;
+
+SELECT customer_id, SUM(amount) FROM payment
+WHERE customer_id NOT IN (184, 87, 477)
+GROUP BY customer_id;
+
+SELECT store_id,COUNT(*) FROM customer
+GROUP BY store_id
+HAVING COUNT(*) > 300;
+
+
+
+
